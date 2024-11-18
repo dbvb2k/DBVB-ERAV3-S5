d79a984ec7b1ca9fb7a55ff4b03ed70a321a8ce6
Venkatesh Babu <dbvb2k@gmail.com>
2024-11-18 10:04:08 +0530

    Changed model to reduce the parameters to < 10000

--- model.py
+++ model.py
@@ -1,19 +1,82 @@
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 class MNISTModel(nn.Module):
     def __init__(self):
         super(MNISTModel, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(10, 10, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(10 * 7 * 7, 128)
-        self.fc2 = nn.Linear(128, 10)
         
+        # First Block
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=3)  # 28x28 -> 26x26
+        self.bn1 = nn.BatchNorm2d(16)
+        self.relu1 = nn.ReLU()
+        self.dropout1 = nn.Dropout(0.25)
+        
+        # Second Block
+        self.conv2 = nn.Conv2d(16, 16, kernel_size=3)  # 26x26 -> 24x24
+        self.bn2 = nn.BatchNorm2d(16)
+        self.relu2 = nn.ReLU()
+        self.dropout2 = nn.Dropout(0.25)
+        
+        # First MaxPool
+        self.pool1 = nn.MaxPool2d(2, 2)  # 24x24 -> 12x12
+        
+        # Third Block
+        self.conv3 = nn.Conv2d(16, 32, kernel_size=3)  # 12x12 -> 10x10
+        self.bn3 = nn.BatchNorm2d(32)
+        self.relu3 = nn.ReLU()
+        self.dropout3 = nn.Dropout(0.25)
+        
+        # Second MaxPool
+        self.pool2 = nn.MaxPool2d(2, 2)  # 10x10 -> 5x5
+        
+        # Final Convolution Blocks
+        self.conv4 = nn.Conv2d(32, 10, kernel_size=1)  # 5x5 -> 5x5
+        self.conv5 = nn.Conv2d(10, 10, kernel_size=5)  # 5x5 -> 1x1
+        self.bn4 = nn.BatchNorm2d(10)
+        self.relu4 = nn.ReLU()
+        
+        self.flatten = nn.Flatten()
+        self.softmax = nn.Softmax(dim=1)
+
     def forward(self, x):
-        x = F.max_pool2d(F.relu(self.conv1(x)), 2)
-        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
-        x = x.view(-1, 10 * 7 * 7)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1) 
+        # First Block
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+        x = self.dropout1(x)
+        
+        # Second Block
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu2(x)
+        x = self.dropout2(x)
+        
+        # First MaxPool
+        x = self.pool1(x)
+        
+        # Third Block
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = self.relu3(x)
+        x = self.dropout3(x)
+        
+        # Second MaxPool
+        x = self.pool2(x)
+        
+        # Final Convolutions
+        x = self.conv4(x)
+        x = self.conv5(x)
+        x = self.bn4(x)
+        x = self.relu4(x)
+        
+        # Output
+        x = self.flatten(x)
+        x = self.softmax(x)
+        return x
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+if __name__ == "__main__":
+    model = MNISTModel()
+    total_params = count_parameters(model)
+    print(f"Total trainable parameters: {total_params}") 
